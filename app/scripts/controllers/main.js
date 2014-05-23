5119577b7aa12a2c4d054d910a22a4437baa457a
Minh Tran <gtdminh@gmail.com>
2014-05-23 11:37:23 +0700

    generate angular skeleton with boostrap

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dandelionAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
